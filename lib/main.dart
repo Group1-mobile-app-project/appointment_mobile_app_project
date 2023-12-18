6202ea7ad956f8eb3f458d391fa063ff69736bb0
aran-adham <aranadham1@gmail.com>
2023-12-18 20:22:15 +0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Barber Appointment Appp',
+      title: 'Barber Appointment App',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
